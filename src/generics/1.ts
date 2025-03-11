fc1133874e65c93edff40126c80217df48382b12
artem <artemilchenko28@gmail.com>
2025-03-11 20:11:41 +0100

    ass

--- src/generics/1.ts
+++ src/generics/1.ts
@@ -1,11 +1,13 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-async function fetchData<T>(url: string): Promise<T> {
-  try {
-    const response = await axios.get(url);
-    return response.data;
-  } catch (error) {
-    throw new Error(`Error fetching from ${url}: ${error}`);
-  }
-}
-console.log(fetchData("asd"));
+// async function fetchData<T>(url: string): Promise<T> {
+//   try {
+//     const response = await axios.get(url);
+//     return response.data;
+//   } catch (error) {
+//     throw new Error(`Error fetching from ${url}: ${error}`);
+//   }
+// }
+// console.log(fetchData("asd"));
+
+console.log("hello world");
